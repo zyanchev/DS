695c2fe5ff361f9465f76170302251a8bb6cf35a
zyanchev <132891429+zyanchev@users.noreply.github.com>
2023-05-11 23:32:28 +0300

    Update testDS5.cy.js

--- cypress/e2e/testDS5.cy.js
+++ cypress/e2e/testDS5.cy.js
@@ -1,13 +1,14 @@
 it("Does blur the background after clicking 'Get the app' button", () => {
 
     cy.visit('https://mind-wend-913065.framer.app/')
-    cy.get('.framer-15fwyui')
+    
+    cy.get('.get-app-button')
         .should('be.visible')
         .click()
 
     cy.get('.framer-vrqh0x')
         .should('have.css', 'backdrop-filter')
         .then((blur) => {
-            expect(blur).to.eq('blur(20px)')
+            expect(blur).to.not.equal('blur(0px)')
         })
 })
