d98a086aa383e39905ff337fe38e75c95078fefa
zyanchev <132891429+zyanchev@users.noreply.github.com>
2023-05-11 22:47:50 +0300

    Update testDS2.cy.js

--- cypress/e2e/testDS2.cy.js
+++ cypress/e2e/testDS2.cy.js
@@ -2,9 +2,9 @@ it("Does contain 4 questions in the FAQ section from'Pricing' page", () => {
 
     cy.visit('https://mind-wend-913065.framer.app/')
 
-    cy.get('.framer-1g3amks-container > .framer-WalAV > .framer-1892sln > .framer-1gmmtpa > .framer-styles-preset-kqguaa > span').should("be.visible").click()
+    cy.get('[data-framer-name="Desktop"]').contains('Pricing').should("be.visible").click()
 
-    cy.get('.framer-eabl6y')
+    cy.contains('FAQ').parent().parent().siblings()
         .should('be.visible')
         .then((faqQuestions) => {
             const questionsLength = faqQuestions[0].childElementCount;
