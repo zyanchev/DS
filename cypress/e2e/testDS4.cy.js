e08cadd84eb58afcc2dbba38ec347d58212260fa
zyanchev <132891429+zyanchev@users.noreply.github.com>
2023-05-11 23:19:43 +0300

    Update testDS4.cy.js

--- cypress/e2e/testDS4.cy.js
+++ cypress/e2e/testDS4.cy.js
@@ -2,12 +2,12 @@ it("Does display 'Sign up' button's color as 'rgb(255,82,79)'", () => {
 
     cy.visit('https://mind-wend-913065.framer.app/')
 
-    cy.scrollTo("bottom")
+    cy.get('[data-framer-name="Footer"]').scrollIntoView()
 
-    cy.get('[style="position:relative"] > input')
+    cy.get('[data-framer-name="Footer"] [type="submit"]')
         .should('be.visible')
         .invoke('css', 'background-color')
         .then((color) => {
             expect(color).to.eq('rgb(255, 82, 79)')
-        })
+        })  
 })
